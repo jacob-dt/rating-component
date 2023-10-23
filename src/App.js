442e095e638e4a397e7c46f614dabc0c7840b434
jacob-dt <107429249+jacob-dt@users.noreply.github.com>
2023-10-23 01:42:55 -0400

    update App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
 
 function App() {
-  
+  return (
+    <div className = "card">
+
+      <span className = "symbol">&#9733;</span>
+
+      <div className= "card-text">
+        <h1>How did we do?</h1>
+        <p>Please let us know how we did 
+          with your support request. All feedback 
+          is appreciated to help us imporve our offering!
+        </p>
+      </div>
+
+      <div className="buttons">
+        <ul className="ratings">
+          <li><button>1</button></li>
+          <li><button>2</button></li>
+          <li><button>3</button></li>
+          <li><button>4</button></li>
+          <li><button>5</button></li>
+        </ul>
+        <button className="submit" type='submit'>Sumbit</button>
+      </div>
+
+    </div>
+  )
 }
 
 export default App;
