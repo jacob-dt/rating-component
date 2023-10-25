06de9b392c99d7dd019d340012bd6254fa61a0bc
jacob-dt <107429249+jacob-dt@users.noreply.github.com>
2023-10-24 21:27:34 -0400

    make minor adjustments to styling

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <h1>How did we do?</h1>
         <p>Please let us know how we did 
           with your support request. All feedback 
-          is appreciated to help us imporve our offering!
+          is appreciated to help us improve our offering!
         </p>
       </div>
 
